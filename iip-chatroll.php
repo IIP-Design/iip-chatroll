05816f815242551fb695c0061c3ed02a35bf94b3
Scott Gustas <scottius@gmail.com>
2016-07-11 09:16:11 -0400

    Remove license text

--- iip-chatroll.php
+++ iip-chatroll.php
@@ -7,13 +7,6 @@
  * License: GPLv2 or later
  */
 
-
-/*
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
-
 define( 'IIP_CHATROLL_URL', plugin_dir_url(__FILE__) );
 
 add_action('init', 'iip_chatroll_scripts');
