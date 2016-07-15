3c1d221d6ef328e46da201d6f8dbaccf1405424b
Scott Gustas <scottius@gmail.com>
2016-07-15 15:01:32 -0400

    Add reduced size when minimized

--- js/iip-chatroll.js
+++ js/iip-chatroll.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function($){
   $('.iip_toggle').click(function(){
     $('.iip_two').toggleClass('maximized');
     $('.iip_chatroll iframe').toggle();
+    $('.chatroll_topbar').toggleClass('reduced');
   });
 });
